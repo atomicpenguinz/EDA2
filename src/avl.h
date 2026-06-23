4b207e4cee8460e5ad219fff2b2fcdd33df5828c
atomicpenguinz <heitorlcaetano@gmail.com>
2026-06-22 23:42:36 -0300

    adicionando protótipos

--- src/avl.h
+++ src/avl.h
@@ -15,3 +15,17 @@ typedef struct arvore {
 	struct nodo *raiz;
 } ArvoreAVL;
 
+/* avl.c */
+ArvoreAVL *criar_arvore(void);
+NoAVL *criar_nodo(NoAVL *pai, int chave);
+int altura(NoAVL *no);
+void att_altura(NoAVL *no);
+int fb(NoAVL *no);
+NoAVL *rse(ArvoreAVL *arvore, NoAVL *no);
+NoAVL *rsd(ArvoreAVL *arvore, NoAVL *no);
+NoAVL *rde(ArvoreAVL *arvore, NoAVL *no);
+NoAVL *rdd(ArvoreAVL *arvore, NoAVL *no);
+NoAVL *adicionar_no(ArvoreAVL *arvore, NoAVL *no, int chave);
+void balanceamento(ArvoreAVL *arvore, NoAVL *no);
+NoAVL *inserir(ArvoreAVL *arvore, int valor);
+NoAVL *remover(ArvoreAVL *arvore, int valor);
